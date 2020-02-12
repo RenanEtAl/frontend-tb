7adc5fc7c700a335158629a94805d906211a509f
Renanie Tanola <enan.tanola@gmail.com>
2020-02-12 11:29:38 -0800

    product page and related products

--- front-tb/src/core/Home.js
+++ front-tb/src/core/Home.js
@@ -35,15 +35,19 @@ const Home = () => {
 
     return (
         <Layout title="Home page" description="Node react" className="container-fluid">
-        <Search />
+            <Search />
             <h2 className="mb-4">New Arrivals</h2>
             <div className="row">
-                {productsByArrival.map((product, index) => (<Card key={index} product={product} />))}
+                {productsByArrival.map((product, index) => (<div key={index} className="col-4 mb-3">
+                    <Card product={product} />
+                </div>))}
             </div>
 
             <h2 className="mb-4">Best Sellers</h2>
             <div className="row">
-                {productsBySell.map((product, index) => (<Card key={index} product={product} />))}
+                {productsBySell.map((product, index) => (<div key={index} className="col-4 mb-3">
+                    <Card product={product} />
+                </div>))}
             </div>
 
         </Layout>
